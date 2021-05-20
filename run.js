132d1efd65d95d81773c695e16464926f47fc0a6
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-05-20 12:19:26 -0700

    run more frameworks

--- run.js
+++ run.js
@@ -1,30 +1,35 @@
 const child_process = require("child_process");
 const path = require("path");
 
-const keyed = [
+const frameworks = [
   "keyed/1more",
   "keyed/angular",
+  "keyed/aurelia",
   "keyed/boredom",
+  "keyed/elm",
+  "keyed/ember",
   "keyed/fluid",
+  "keyed/glimmer",
+  "keyed/hyperapp",
   "keyed/hyperhtml",
+  "keyed/imba",
+  "keyed/incinerate",
+  "keyed/inferno",
+  "keyed/ivi",
+  "keyed/lighterhtml",
   "keyed/lit-html",
+  "keyed/marko",
+  "keyed/preact",
   "keyed/react",
+  "keyed/react-hooks",
+  "keyed/redom",
+  "keyed/riot",
+  "keyed/sinuous",
   "keyed/solid",
+  "keyed/stencil",
+  "keyed/svelte",
   "keyed/vanillajs",
   "keyed/vue",
-];
-
-const nonKeyed = [
-  "non-keyed/angular",
-  "non-keyed/aurelia",
-  "non-keyed/elm",
-  "non-keyed/fluid",
-  "non-keyed/lighterhtml",
-  "non-keyed/lit-html",
-  "non-keyed/react",
-  "non-keyed/svelte",
-  "non-keyed/vanillajs",
-  "non-keyed/vue",
 ];
 
 const message = (value) =>
@@ -49,8 +54,8 @@ const execute = (command, options) => {
   }
 };
 
-if (process.argv.includes("build-keyed")) {
-  for (const framework of keyed) {
+if (process.argv.includes("build")) {
+  for (const framework of frameworks) {
     message(`BUILDING ${framework}`);
 
     const options = {
@@ -62,36 +67,13 @@ if (process.argv.includes("build-keyed")) {
     execute("npm audit fix", options);
     execute("npm run build-prod", options);
   }
-} else if (process.argv.includes("build-non-keyed")) {
-  for (const framework of nonKeyed) {
-    message(`BUILDING ${framework}`);
-
-    const options = {
-      cwd: path.join(process.cwd(), "frameworks", framework),
-      stdio: "inherit",
-    };
-
-    execute("npm install", options);
-    execute("npm audit fix", options);
-    execute("npm run build-prod", options);
-  }
-} else if (process.argv.includes("bench-keyed")) {
+} else if (process.argv.includes("bench")) {
   const options = {
     cwd: path.join(process.cwd(), "webdriver-ts"),
     stdio: "inherit",
   };
 
-  for (const framework of keyed) {
-    message(`RUNNING ${framework}`);
-    execute(`npm run bench ${framework}`, options);
-  }
-} else if (process.argv.includes("bench-non-keyed")) {
-  const options = {
-    cwd: path.join(process.cwd(), "webdriver-ts"),
-    stdio: "inherit",
-  };
-
-  for (const framework of nonKeyed) {
+  for (const framework of frameworks) {
     message(`RUNNING ${framework}`);
     execute(`npm run bench ${framework}`, options);
   }
